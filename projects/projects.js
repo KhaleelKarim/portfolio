c4e311ee13a47366bad9718ec44a34770934c6db
Khaleel Karim <kkarim@ucsd.edu>
2025-05-10 22:31:51 -0700

    first step

--- projects/projects.js
+++ projects/projects.js
@@ -6,50 +6,97 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
-// Make pie chart
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let projectsJSON = await fetchJSON('../lib/projects.json'); // fetch your project data
-let rolledData = d3.rollups(
-  projectsJSON,
-  (v) => v.length,
-  (d) => d.year,
-);
-
-console.log(rolledData)
-
-let data = rolledData.map(([year, count]) => {
-    return { value: count, label: year };
-});
-let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
-
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
-
-arcs.forEach((arc, idx) => {
-    d3.select('svg').append('path')
-      .attr('d', arc).attr('fill', colors(idx));
-});
-
-// Create legend
-let legend = d3.select('.legend');
-data.forEach((d, idx) => {
-  legend
-    .append('li')
-    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
-});
 
 // Add search functionality
 let query = '';
 
 let searchInput = document.querySelector('.searchBar');
 
+
+// Refactor all plotting into one function
+function renderPieChart(projectsGiven) {
+
+  let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+  // re-calculate rolled data
+  let newRolledData = d3.rollups(
+    projectsGiven,
+    (v) => v.length,
+    (d) => d.year,
+  );
+  // re-calculate data
+  let newData = newRolledData.map(([year, count]) => {
+    return { value: count, label: year };; // TODO
+  });
+  // re-calculate slice generator, arc data, arc, etc.
+  let newSliceGenerator = d3.pie().value((d) => d.value);
+  let newArcData = newSliceGenerator(newData);
+  let newArcs = newArcData.map((d) => arcGenerator(d));
+  // TODO: clear up paths and legends
+  d3.select('.legend') // Select the <ul> element with the class "legend"
+  .selectAll('li')   // Select all <li> elements inside it
+  .remove();         // Remove all the selected <li> elements
+
+  let newSVG = d3.select('svg');
+  newSVG.selectAll('path').remove();;
+
+  // update paths and legends, refer to steps 1.4 and 2.2
+  let colors = d3.scaleOrdinal(d3.schemeTableau10);
+  newArcs.forEach((arc, idx) => {
+      d3.select('svg').append('path')
+        .attr('d', arc).attr('fill', colors(idx));
+  });
+
+  // Create legend
+  let newLegend = d3.select('.legend');
+  newData.forEach((d, idx) => {
+    newLegend
+      .append('li')
+      .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+  });
+
+  
+}
+
+renderPieChart(projects);
+
 searchInput.addEventListener('change', (event) => {
   // update query value
   query = event.target.value;
-  // TODO: filter the projects
+
+  // filter the projects
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
 
   // TODO: render updated projects!
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+  renderPieChart(filteredProjects);
+  
 });
+
+// let selectedIndex = -1;
+
+// let svg = d3.select('svg');
+// svg.selectAll('path').remove();
+// arcs.forEach((arc, i) => {
+//   svg
+//     .append('path')
+//     .attr('d', arc)
+//     .attr('fill', colors(i))
+//     .on('click', () => {
+//       // What should we do? (Keep scrolling to find out!)
+//       selectedIndex = selectedIndex === i ? -1 : i;
+
+//       svg
+//         .selectAll('path')
+//         .attr('class', (_, idx) => (
+//           // TODO: filter idx to find correct pie slice and apply CSS from above
+//           idx === selectedIndex ? 'highlighted' : ''
+//         ));
+
+//     });
+    
+// });
